0b8a38ad044e0732f8f08e42a3b2882f7a950f8f
Portisch <hugo.portisch@yahoo.de>
2023-01-06 22:22:34 +0100

    WinSystemAmlogic: add support of 'IsHDRDisplay'

--- xbmc/windowing/amlogic/WinSystemAmlogic.h
+++ xbmc/windowing/amlogic/WinSystemAmlogic.h
@@ -33,6 +33,8 @@ public:
   bool DestroyWindow() override;
   void UpdateResolutions() override;
   bool CanDoWindowed() override { return false; }
+  bool IsHDRDisplay() override;
+  CHDRCapabilities GetDisplayHDRCapabilities() const override;
 
   bool Hide() override;
   bool Show(bool show = true) override;
@@ -54,4 +56,5 @@ protected:
   CCriticalSection m_resourceSection;
   std::vector<IDispResource*> m_resources;
   std::unique_ptr<CLibInputHandler> m_libinput;
+  CHDRCapabilities hdr_caps;
 };
