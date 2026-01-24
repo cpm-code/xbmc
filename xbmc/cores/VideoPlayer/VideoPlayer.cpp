1b02ec0ae80cf36aaf55058f204504b0fd3c0020
cpm <162544069+cpm-code@users.noreply.github.com>
2026-01-24 21:36:44 +0800

    pgs: use demuxed video hdrType for matrix/bypass

--- xbmc/cores/VideoPlayer/VideoPlayer.cpp
+++ xbmc/cores/VideoPlayer/VideoPlayer.cpp
@@ -3910,6 +3910,7 @@ bool CVideoPlayer::OpenStream(CCurrentStream& current, int64_t demuxerId, int iS
       res = OpenVideoStream(hint, reset);
       break;
     case StreamType::SUBTITLE:
+      hint.hdrType = m_CurrentVideo.hint.hdrType; // Set by Video Stream which is opened first
       res = OpenSubtitleStream(hint);
       break;
     case StreamType::TELETEXT:
