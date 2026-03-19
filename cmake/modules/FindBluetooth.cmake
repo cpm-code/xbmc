8a7a01b47984c0ebe6e206d42569ff285fc85dc7
cpm <162544069+cpm-code@users.noreply.github.com>
2026-03-19 22:18:31 +0800

    [cmake] fix bluetooth linking with stripped bluez.pc

--- cmake/modules/FindBluetooth.cmake
+++ cmake/modules/FindBluetooth.cmake
@@ -24,6 +24,23 @@ if(NOT TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
   if(${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME}_FOUND)
     add_library(${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIAS PkgConfig::${${CMAKE_FIND_PACKAGE_NAME}_SEARCH_NAME})
 
+    get_target_property(_ALIASTARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME} ALIASED_TARGET)
+    if(_ALIASTARGET)
+      set(LIB_TARGET ${_ALIASTARGET})
+    else()
+      set(LIB_TARGET ${APP_NAME_LC}::${CMAKE_FIND_PACKAGE_NAME})
+    endif()
+
+    get_target_property(_bluetooth_link_libs ${LIB_TARGET} INTERFACE_LINK_LIBRARIES)
+    if(NOT _bluetooth_link_libs MATCHES "(^|;)bluetooth($|;)" AND
+       NOT _bluetooth_link_libs MATCHES "(^|;)LIBRARY::bluetooth($|;)" AND
+       NOT _bluetooth_link_libs MATCHES "libbluetooth")
+      find_library(BLUETOOTH_LIBRARY NAMES bluetooth)
+      if(BLUETOOTH_LIBRARY)
+        target_link_libraries(${LIB_TARGET} INTERFACE "${BLUETOOTH_LIBRARY}")
+      endif()
+    endif()
+
     set(${${CMAKE_FIND_PACKAGE_NAME}_MODULE}_COMPILE_DEFINITIONS HAVE_LIBBLUETOOTH)
     ADD_TARGET_COMPILE_DEFINITION()
   endif()
