581f6d5306dc3d428b4155a6404b9ab14ba7b3cd
cpm <162544069+cpm-code@users.noreply.github.com>
2026-05-07 23:02:39 +0800

    GUIInfoManager: fix TranslateTimeFormat indentation

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -11686,8 +11686,8 @@ TIME_FORMAT CGUIInfoManager::TranslateTimeFormat(const std::string &format)
   if (format.empty())
     return TIME_FORMAT_GUESS;
 
-    if (const auto* timeFormat = FindCaseInsensitiveValue(format, time_formats))
-        return *timeFormat;
+  if (const auto* timeFormat = FindCaseInsensitiveValue(format, time_formats))
+    return *timeFormat;
 
   return TIME_FORMAT_GUESS;
 }
